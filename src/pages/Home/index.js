ac6e296c003d44db6b41b371cf746f89578c8ce8
Davi Holanda <daviht7@gmail.com>
2022-03-05 20:03:03 -0300

    criando pagina inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-
 function Home() {
 
-    return <div>Home</div>
+    return (
+        <div>
+
+            <header className="header">
+                <div className="toolbar">
+                    <div className="">
+                        <span>Conecta Dev</span>
+                    </div>
+                    <div className="">
+                        <button>Novo Post</button>
+                        <span>img1</span>
+                        <span>img2</span>
+                    </div>
+                </div>
+            </header>
+            <main>
+                main
+            </main>
+        </div>
+    )
 }
 
 export default Home;
