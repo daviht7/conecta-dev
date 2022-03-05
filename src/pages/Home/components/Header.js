250e31e7eab798a43b3ce0e6152732845e13a8bc
Davi Holanda <daviht7@gmail.com>
2022-03-05 20:15:30 -0300

    componente da pagina Home

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,21 @@
+
+function Header() {
+
+    return (
+        <header className='header'>
+            <div className='toolbar'>
+                <div>
+                    <a href="/">Conecta Dev</a>
+                </div>
+                <div>
+                    <button>Novo Post</button>
+                    <span>img1</span>
+                    <span>img2</span>
+                </div>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,18 @@
+import Header from './components/Header';
+import './style.css';
+
 function Home() {
 
     return (
         <div>
-
-            <header className="header">
-                <div className="toolbar">
-                    <div className="">
-                        <span>Conecta Dev</span>
-                    </div>
-                    <div className="">
-                        <button>Novo Post</button>
-                        <span>img1</span>
-                        <span>img2</span>
-                    </div>
+            <Header/>
+            <main className='main'>
+                <div className='navbar'>
+                    navbar
                 </div>
-            </header>
-            <main>
-                main
+                <div className='feed'>
+                    feed
+                </div>
             </main>
         </div>
     )
